3d26deba653b95e5afc3da04692a595937769266
Bella Fenggit config --global user.email bellafeng@gmail.comgit config --global user.name "Bella Feng"git config --global user.email bellafeng@gmail.comgit config --global credential.help wincredgit --creditialsgit config --global user.name Bella <bellafeng@gmail.com>
2018-03-14 11:10:11 -0700

    adding regression

---
+++ regression.R
@@ -0,0 +1,13 @@
+library(rpart)
+library(rpart.plot)
+source("split_data_grade.R")
+# Train the model
+grade_model <- rpart(formula = final_grade ~ ., 
+                     data = grade_train, 
+                     method = "anova")
+
+# Look at the model output                      
+print(grade_model)
+
+# Plot the tree model
+rpart.plot(x = grade_model, yesno = 2, type = 0, extra = 0)
