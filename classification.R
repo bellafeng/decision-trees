f789cbd29d218392b5a115f1ef05319abf43f786
Bella Feng <bellafeng@gmail.com>
2018-03-14 06:30:16 -0700

    classification tree added

---
+++ classification.R
@@ -0,0 +1,45 @@
+library(rpart)
+# Train the model (to predict 'default')
+credit_model <- rpart(formula = default~., 
+                      data = credit_train, 
+                      method = "class")
+
+# Look at the model output                      
+print(credit_model)
+
+# Generate predicted classes using the model object
+class_prediction <- predict(object = credit_model,  
+                            newdata = credit_test,   
+                            type = "class")  
+
+# Calculate the confusion matrix for the test set
+confusionMatrix(data = class_prediction,       
+                reference = credit_test$default)  
+
+# Train a gini-based model
+credit_model1 <- rpart(formula = default ~ ., 
+                       data = credit_train, 
+                       method = "class",
+                       parms = list(split = "gini"))
+
+# Train an information-based model
+credit_model2 <- rpart(formula = default ~ ., 
+                       data = credit_train, 
+                       method = "class",
+                       parms = list(split = "information"))
+
+# Generate predictions on the validation set using the gini model
+pred1 <- predict(object = credit_model1, 
+                 newdata = credit_test,
+                 type = "class")    
+
+# Generate predictions on the validation set using the information model
+pred2 <- predict(object = credit_model2, 
+                 newdata = credit_test,
+                 type = "class")
+
+# Compare classification error
+ce(actual = credit_test$default, 
+   predicted = pred1)
+ce(actual = credit_test$default, 
+   predicted = pred2)  
